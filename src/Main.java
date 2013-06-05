b6958c0ccdead9612c708918a6020a2471ecf494
Martin McAllister <marty@that-london.com>
2013-06-05 19:20:13 +0100

    Handling fields which are too long. Looks like we were truncating data anyway!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import au.com.bytecode.opencsv.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Main {
@@ -16,6 +18,10 @@ public class Main {
 
             for (File child : dir.listFiles(getCSVFilter()))
             {
+                ArrayList<Integer> fieldsAndLength = CleverSizedTableCreator(child);
+
+
+
                 String tableName = child.getName().replace(".csv", "");
                 int count = 0;
 
@@ -25,24 +31,26 @@ public class Main {
                 StringBuilder insertData = new StringBuilder();
 
                 String [] nextLine;
-                while ((nextLine = reader.readNext()) != null) {
-
-
+                while ((nextLine = reader.readNext()) != null)
+                {
                     if (count == 0)
                     {
                         //make a table script...
-                        MakeTable(createTable, nextLine, tableName);
+                        MakeTable(createTable, nextLine, tableName, fieldsAndLength);
                         count++;
                     }
                     else
                     {
                         InsertData(insertData, nextLine, tableName);
+                        count++;
                     }
                 }
 
                 //write out the stuff.
                 writeFile(tableName, createTable, "CreateTable.sql");
                 writeFile(tableName, insertData, "InsertData.sql");
+
+                System.out.print(count + " rows to insert!\n");
             }
         }
         catch (IOException ex)
@@ -80,6 +88,60 @@ public class Main {
         return CSVFilter;
     }
 
+
+    private static ArrayList CleverSizedTableCreator(File csvFile) throws IOException
+    {
+        CSVReader reader = new CSVReader(new FileReader(csvFile));
+        String [] nextLine;
+        ArrayList fieldsAndLength = new ArrayList();
+
+        int count = 0;
+        while ((nextLine = reader.readNext()) != null)
+        {
+
+            if (count == 0)
+            {
+                //populate hashmap keys
+                PopulateHashMapKeys(fieldsAndLength, nextLine);
+
+            }
+            else
+            {
+                //populate hahmap values
+                PopulateHashMapLengths(fieldsAndLength, nextLine);
+            }
+            count++;
+
+
+
+        }
+        return fieldsAndLength;
+    }
+
+    private static void PopulateHashMapKeys(ArrayList<Integer> fieldsAndLength, String[] nextLine)
+    {
+        for (int i=0; i<nextLine.length; i++ )
+        {
+            //fieldsAndLength.put(nextLine[i], 0);
+            fieldsAndLength.add(i, 1);
+        }
+    }
+
+    private static void PopulateHashMapLengths(ArrayList<Integer> fieldsAndLength, String[] nextLine)
+    {
+        for (int i=0; i<nextLine.length; i++ )
+        {
+            //Integer length = fieldsAndLength.get(nextLine[i]);
+            if (fieldsAndLength.get(i) < nextLine[i].length())
+            {
+                //fieldsAndLength.put(nextLine[i], nextLine[i].length());
+                fieldsAndLength.set(i, nextLine[i].length());
+            }
+        }
+    }
+
+
+
     private static void InsertData(StringBuilder sqlString, String[] nextLine, String tableName)
     {
         for (int i=0; i<nextLine.length; i++ )
@@ -98,18 +160,33 @@ public class Main {
         sqlString.append(");\n");
     }
 
-    private static void MakeTable(StringBuilder sqlString, String[] nextLine, String tableName)
+    private static void MakeTable(StringBuilder sqlString, String[] nextLine, String tableName, ArrayList<Integer> fieldsAndLength)
     {
         sqlString.append("create table " + tableName + " (");
         for (int i=0; i<nextLine.length; i++ )
         {
             if (i==0)
             {
-                sqlString.append("`" + nextLine[i] + "` varchar(250)\n");
+                if (fieldsAndLength.get(i) < 254)
+                {
+                    sqlString.append("`" + nextLine[i] + "` varchar(" + fieldsAndLength.get(i) + ")\n");
+                }
+                else
+                {
+                    sqlString.append("`" + nextLine[i] + "` text\n");
+                }
+
             }
             else
             {
-                sqlString.append(",`" + nextLine[i] + "` varchar(250)\n");
+                if (fieldsAndLength.get(i) < 254)
+                {
+                    sqlString.append(",`" + nextLine[i] + "` varchar(" + fieldsAndLength.get(i) + ")\n");
+                }
+                else
+                {
+                    sqlString.append(",`" + nextLine[i] + "` text\n");
+                }
             }
         }
         sqlString.append(");\n");
